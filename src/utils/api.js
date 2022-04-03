e55188e6331714a690f941ceea344898da49dad8
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-04-03 17:56:01 -0300

    adiciona a url para cotação do bitcoin

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,4 @@
 const baseUrl = "https://pokeapi.co/api/v2/pokemon/";
+const moneyUrl = "https://api.coinbase.com/v2/prices/spot?currency=USD";
 
-export default baseUrl;
+export {baseUrl, moneyUrl};
