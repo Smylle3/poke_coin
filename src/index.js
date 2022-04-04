d4c566f6fb75cd5f611a464e5b1ebb6db60c9a84
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-04-04 15:47:51 -0300

    Limpeza de código

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-/*12:20-14:40 - 16:00-17:22*/
+
 ReactDOM.render(
     <React.StrictMode>
         <App />
