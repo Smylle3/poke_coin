7b1e8e6dc527b749ae1fcc82106340d4def0463f
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-04-08 18:34:03 -0300

    Cria componente de mostrar histórico

---
+++ src/components/history.jsx
@@ -0,0 +1,15 @@
+import { Center } from '@chakra-ui/react'
+import React from 'react'
+
+export default function History() {
+    return (
+        <Center
+            bg="defaultColor.500"
+            color="defaultColor.400"
+            h="50%"
+            w="100%"
+        >
+            Histórico
+        </Center>
+    )
+}
