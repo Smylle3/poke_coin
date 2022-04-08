051129ddd244415481d19ee157306d4c095da70d
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-04-08 18:35:12 -0300

    Faz remanejamento da pagina Profile

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,123 +1,26 @@
 import React, { useEffect, useState } from 'react'
-import { baseUrl, moneyUrl } from 'utils/api'
 import { BsFillArrowDownCircleFill } from 'react-icons/bs'
 import './styles.css'
+import Orders from 'components/orders'
+import History from 'components/history'
+import SearchPokemons from 'context/searchPokemons'
+import { SearchBitcoin } from 'context/searchBitcoin'
 
 export default function Profile() {
     const [pokeName, setPokeName] = useState('')
-    const [pokeNameSale, setPokeNameSale] = useState('')
-    const [pokeList, setpokeList] = useState([])
-    const [pokeFullList, setPokeFullList] = useState([])
+    const [pokeList, setPokeList] = useState([])
+    //const [pokeFullList, setPokeFullList] = useState([])
     const [cotacao, setCotacao] = useState(0)
-    const [pokeValue, setPokeValue] = useState(0)
 
-    /*UseEffect responsavel pela busca dos valor do dolar em bitcoin*/
-    function handleRefresh() {
-        fetch(moneyUrl)
-            .then((response) => response.json())
-            .then((data) => {
-                setCotacao(data.data.amount)
-            })
-        alert('Atualizado!')
-    }
     useEffect(() => {
-        fetch(moneyUrl)
-            .then((response) => response.json())
-            .then((data) => {
-                setCotacao(data.data.amount)
-            })
+        SearchBitcoin(setCotacao)
     }, [])
 
-    /*UseEffect responsável por atualizar o valor da carteira em dolar*/
-    useEffect(() => {
-        var valorUsd = 0
+    function removePokemon(DeleteIndex) {
         for (let index = 0; index < pokeList.length; index++) {
-            valorUsd += parseFloat(pokeList[index].cotacao)
-            setPokeValue(valorUsd.toFixed(5))
-        }
-    }, [pokeFullList.length, pokeList])
-
-    /*Função assincrona responsavel por todo sistema*/
-    async function handleRegister(event) {
-        if (pokeName.length === 0 && pokeNameSale.length === 0) {
-            /*Verifica se os campos dos inputs estão vazios*/
-            alert('Digite o nome do pokemon!')
-        } else {
-            setPokeName('') /*Reseta os valores no input*/
-            setPokeNameSale('')
-
-            if (event.target.id === 'Compra') {
-                /*Verifica se o usuário efetuou uma compra ou uma venda*/
-                fetch(
-                    `${baseUrl}${pokeName}`
-                ) /*Consome a API de pokemon buscando pelo pokemon digitado*/
-                    .then((response) => response.json())
-                    .then((data) => {
-                        console.log(data)
-                        setpokeList((arr) => [
-                            /*Preenche o array com as informações: id, ação feita, nome do pokemon e seu valor em dolar*/
-                            ...arr,
-                            {
-                                id: pokeList.length + 1,
-                                action: 'Buy',
-                                name: data.name,
-                                exp: data.base_experience,
-                                cotacao: (
-                                    data.base_experience *
-                                    0.00000001 *
-                                    cotacao
-                                ).toFixed(5)
-                            }
-                        ])
-                        setPokeFullList((arr) => [
-                            /*Preenche o array de histórico com id, ação feita, nome do pokemon e sua base de experiencia*/
-                            ...arr,
-                            {
-                                id: pokeList.length ^ 8,
-                                action: 'Buy',
-                                name: data.name,
-                                exp: data.base_experience
-                            }
-                        ])
-                    })
-                    .catch((err) =>
-                        alert('Pokemon não encontrado!')
-                    ) /*Informa que o pokemon não foi encontrado*/
-            } else if (event.target.id === 'Venda') {
-                let flag = false
-                for (let index = 0; index < pokeList.length; index++) {
-                    /*Busca pelo pokemon que o usuário deseja vender*/
-                    if (pokeList[index].name === pokeNameSale) {
-                        flag = true
-                        pokeList.splice(
-                            index,
-                            1
-                        ) /*Remove o pokemon do array de pokemons que o usuário possui*/
-                        break
-                    }
-                }
-                if (flag === false) {
-                    alert('Você não possui esse pokemon!')
-                } else {
-                    fetch(`${baseUrl}${pokeNameSale}`)
-                        .then((response) => response.json())
-                        .then((data) => {
-                            setPokeFullList((arr) => [
-                                /*Insere o pokemon vendido no array de histórico com a ação "Sale indicando a venda dele"*/
-                                ...arr,
-                                {
-                                    id: pokeList.length ^ 2,
-                                    action: 'Sale',
-                                    name: data.name,
-                                    exp: data.base_experience
-                                }
-                            ])
-                        })
-                        .catch((err) =>
-                            alert('Pokemon não encontrado!')
-                        )
-                }
-                flag = false
+            if (pokeList[index].name === DeleteIndex.name) {
+                pokeList.splice(index, 1)
+                break
             }
         }
     }
@@ -151,72 +54,34 @@ export default function Profile() {
                         id="Compra"
                         onClick={(e) => {
                             e.preventDefault()
-                            handleRegister(e)
+                            SearchPokemons(
+                                pokeName,
+                                pokeList,
+                                setPokeList
+                            )
                         }}
                     >
                         Comprar
                     </button>
                 </form>
 
-                <form>
-                    <label>Vender pokemon</label>
-                    <input
-                        onChange={(e) =>
-                            setPokeNameSale(e.target.value)
-                        }
-                        value={pokeNameSale}
-                        type="text"
-                        placeholder="Digite o nome do Pokemon"
-                        required
-                    />
-                    <button
-                        id="Venda"
-                        onClick={(e) => {
-                            e.preventDefault()
-                            handleRegister(e)
-                        }}
-                    >
-                        Vender
-                    </button>
-                </form>
                 <a href="#orders" className="scroll">
                     <BsFillArrowDownCircleFill size="40" />
                 </a>
             </div>
             <div className="rightContainer" id="orders">
-                <div className="boxContainer">
-                    <div className="titleDiv">
-                        <h1 className="title">Suas ordens</h1>
-                        <button
-                            onClick={(e) => {
-                                e.preventDefault()
-                                handleRefresh()
-                            }}
-                        >
-                            Atualizar
-                        </button>
-                    </div>
-                    <div className="table">
-                        <div>Nome da ação</div>
-                        <div>Valor (USD)</div>
-                    </div>
-                    <div className="outMap2">
-                        {pokeList.map((pokemon) => (
-                            <div className="inMap2" key={pokemon.id}>
-                                <div>{pokemon.name}</div>
-                                <div>
-                                    ${' '}
-                                    {(
-                                        pokemon.exp *
-                                        0.00000001 *
-                                        cotacao
-                                    ).toFixed(5)}
-                                </div>
-                            </div>
-                        ))}
-                    </div>
+                <Orders
+                    pokeName={pokeName}
+                    pokeList={pokeList}
+                    setPokeList={setPokeList}
+                    cotacao={cotacao}
+                    setCotacao={setCotacao}
+                    onClick={removePokemon}
+                />
+                <History />
+                {/*<div className="boxContainer">
                     Valor total da sua carteira: $ {pokeValue}
-                </div>
+                    </div>
                 <div className="boxContainer2">
                     <h1 className="title">
                         Histórico de compras e vendas
@@ -235,7 +100,7 @@ export default function Profile() {
                             </div>
                         ))}
                     </div>
-                </div>
+                </div>*/}
             </div>
         </div>
     )
