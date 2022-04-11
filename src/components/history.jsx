c748b257610e861c30d9d0b2426d92a508769eef
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-04-11 20:20:56 -0300

    Cria o componente que mostra o histórico

--- src/components/history.jsx
+++ src/components/history.jsx
@@ -1,15 +1,55 @@
-import { Center } from '@chakra-ui/react'
+import { Center, Flex, Heading } from '@chakra-ui/react'
+import { useAuth } from 'context/auth'
 import React from 'react'
+import MyTable from './myTable'
 
 export default function History() {
+    const { pokemonHistory } = useAuth()
+
     return (
-        <Center
+        <Flex
             bg="defaultColor.500"
-            color="defaultColor.400"
-            h="50%"
-            w="100%"
+            h="mid"
+            w="full"
+            flexDirection="column"
+            alignItems="flex-start"
+            padding="20px 10px"
         >
-            Histórico
-        </Center>
+            <Center marginBottom={5}>
+                <Heading fontFamily="heading" color="defaultColor.400">
+                    Histórico de compras
+                </Heading>
+            </Center>
+            <MyTable
+                Avatar={null}
+                Nome={null}
+                Action={null}
+                Valor="Base Exp"
+                BGColor="defaultColor.400"
+                Color="defaultColor.500"
+                Width="100%"
+            />
+            <Flex
+                direction="column-reverse"
+                overflowY="scroll"
+                h="90%"
+                w="100%"
+                maxH="90%"
+                margin="10px 0px"
+            >
+                {pokemonHistory.map((pokemon) => (
+                    <Flex alignItems="center" key={pokemon.id}>
+                        <MyTable
+                            Avatar={pokemon.avatar}
+                            Nome={pokemon.name}
+                            Action={pokemon.action}
+                            Valor={pokemon.exp}
+                            Color="defaultColor.400"
+                            Width="100%"
+                        />
+                    </Flex>
+                ))}
+            </Flex>
+        </Flex>
     )
 }
