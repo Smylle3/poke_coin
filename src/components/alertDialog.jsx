802b24c53984035605f1d9995bca35d9cea6e8dc
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-04-11 22:38:53 -0300

    Componente de alerta de venda de pokemon

---
+++ src/components/alertDialog.jsx
@@ -0,0 +1,68 @@
+import {
+    AlertDialog,
+    AlertDialogBody,
+    AlertDialogContent,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogOverlay,
+    Button
+} from '@chakra-ui/react'
+import { useAuth } from 'context/auth'
+import { DeletePokemons } from 'functions/deletePokemons'
+import React from 'react'
+
+export default function AlertSale(props) {
+    const { pokemonList, setPokemonHistory } = useAuth()
+    const cancelRef = React.useRef()
+
+    function deletePokemon() {
+        props.onClose()
+        DeletePokemons(
+            props.pokeNameSell,
+            pokemonList,
+            setPokemonHistory
+        )
+    }
+    
+    return (
+        <>
+            <AlertDialog
+                isOpen={props.isOpen}
+                leastDestructiveRef={cancelRef}
+                onClose={props.onClose}
+            >
+                <AlertDialogOverlay>
+                    <AlertDialogContent>
+                        <AlertDialogHeader
+                            fontSize="lg"
+                            fontWeight="bold"
+                        >
+                            Vender Pokemon
+                        </AlertDialogHeader>
+
+                        <AlertDialogBody>
+                            Tem certeza que deseja vender seu{' '}
+                            {props.pokeNameSell.name}?
+                        </AlertDialogBody>
+
+                        <AlertDialogFooter>
+                            <Button
+                                ref={cancelRef}
+                                onClick={props.onClose}
+                            >
+                                Cancelar
+                            </Button>
+                            <Button
+                                colorScheme="red"
+                                onClick={deletePokemon}
+                                ml={3}
+                            >
+                                Vender
+                            </Button>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialogOverlay>
+            </AlertDialog>
+        </>
+    )
+}
