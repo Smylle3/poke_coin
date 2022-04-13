a286f45f8070c4c300af9b6a8b739f29d4c24c59
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-04-13 19:05:44 -0300

    Faz a configuração do banco de dados do firebase

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, GithubAuthProvider, TwitterAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+}
+
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+const gitProvider = new GithubAuthProvider()
+const twitterProvider = new TwitterAuthProvider()
+
+export {auth, googleProvider, gitProvider, twitterProvider}
+export default app
