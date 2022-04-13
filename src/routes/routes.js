b2ec4ece7cbbf897a9bbcd61168f54429f048e59
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-04-13 19:00:48 -0300

    Cria um arquivo para as rotas e configuração delas

---
+++ src/routes/routes.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import Profile from 'pages/profile'
+import Login from 'pages/login'
+import SignUp from 'pages/cadastro'
+import ProtectedRoutes from './protectedRoutes'
+
+function AppRoutes() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    exact
+                    path="/"
+                    element={
+                        <ProtectedRoutes>
+                            <Profile />
+                        </ProtectedRoutes>
+                    }
+                />
+                <Route path="/login" element={<Login />} />
+                <Route path="/cadastro" element={<SignUp />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default AppRoutes
