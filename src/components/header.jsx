096ae2cf3ce5630ae221202db7ddbbe279189ebf
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-04-14 18:58:34 -0300

    Faz a configuração e a estilização da header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,30 +1,46 @@
-import { Button, Flex, Text } from '@chakra-ui/react'
+import { Box, Flex, Image, Text } from '@chakra-ui/react'
 import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { useAuth } from 'context/authContext'
+import pokemonLogo from 'assets/logoImages/pokemonLogo.png'
+import bitcoinLogo from 'assets/logoImages/bitcoinLogo.png'
+import Options from './options'
+import useMobile from 'utils/useMobile'
 
 export default function Header() {
-    const { user, logOut } = useAuth()
-    const navigate = useNavigate()
-
-    const handleLogOut = async () => {
-        try {
-            await logOut()
-            navigate('/login')
-        } catch (error) {
-            console.log(error.message)
-        }
-    }
+    const { user } = useAuth()
+    const isMobile = useMobile()
 
     return (
-        <Flex w="100%" bg="defaultColor.500" color="defaultColor.400">
-            <Button onClick={handleLogOut} colorScheme="blue">
-                <Link to={'/login'}>Login </Link>
-            </Button>
-            <Text>User Email: {user && user.email}</Text>
-            <Button onClick={handleLogOut} colorScheme="blue">
-                LogOut
-            </Button>
-        </Flex>
+        <Box
+            w="100%"
+            bg="defaultColor.500"
+            color="defaultColor.400"
+            h="7%"
+            position="fixed"
+            zIndex={999}
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+        >
+            <Flex
+                direction="row"
+                h="100%"
+                w="160px"
+                alignItems="center"
+                padding="5px 5px 5px 10px"
+            >
+                <Link to="/">
+                    <Flex direction="row">
+                        <Image h={10} alt="Pokemon" src={pokemonLogo} />
+                        <Image h={9} alt="Bitcoin" src={bitcoinLogo} />
+                    </Flex>
+                </Link>
+            </Flex>
+            <Text>Olá {user.displayName}!</Text>
+            <Flex padding="0px 10px 0px 0px">
+                <Options isMobile={isMobile} />
+            </Flex>
+        </Box>
     )
 }
