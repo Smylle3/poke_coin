199e6553813f404d259b4395512aa0cae5461fa5
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-04-15 12:01:42 -0300

    Cria layout basico responsivo

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,9 +1,38 @@
+import { Flex, Image } from '@chakra-ui/react'
+import Header from 'components/header'
+import { useAuth } from 'context/authContext'
 import React from 'react'
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
+    const {user} = useAuth()
+    return (
+        <Flex w="100%" h="100vh" direction="column">
+            <Header />
+            <Flex h="7%" />
+            <Flex
+                w="100%"
+                h="93%"
+                bg="defaultColor.400"
+                display={{ md: 'flex' }}
+                justifyContent="center"
+            >
+                <Flex
+                    w={{ base: 'full', md: '70%' }}
+                    h="100%"
+                    bg="defaultColor.500"
+                    justifyContent="center"
+                    padding={10}
+                >
+                    <Image
+                        borderRadius="full"
+                        boxSize="200px"
+                        src={user.photoURL}
+                        alt={user.displayName}
+                    />
+                </Flex>
+            </Flex>
+        </Flex>
+    )
 }
 
 export default Profile
