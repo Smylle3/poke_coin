e48e6d5cc97fcc9d11ca5d265fc5098f258fe733
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-04-15 15:25:17 -0300

    Config a mudança de user name e provedor do user

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -22,7 +22,7 @@ import { BiEditAlt } from 'react-icons/bi'
 import MyToast from 'components/myToast'
 
 const Profile = () => {
-    const { user } = useAuth()
+    const { user, providerUser } = useAuth()
     const [userName, setUserName] = useState('')
     const [editProfile, setEditProfile] = useState(null)
     const [oldPassword, setOldPassword] = useState('')
@@ -37,7 +37,11 @@ const Profile = () => {
         } else {
             try {
                 await updateUser(userName)
-                MyToast(toast, 'Nome de usuário alterado com sucesso!', 'success')
+                MyToast(
+                    toast,
+                    'Nome de usuário alterado com sucesso!',
+                    'success'
+                )
                 setEditProfile(null)
             } catch (error) {
                 console.log(error)
@@ -72,9 +76,13 @@ const Profile = () => {
     }
 
     return (
-        <Flex w="100%" h="100%" direction="column">
+        <Flex
+            w="100%"
+            h={providerUser === 'password' ? '100%' : '100vh'}
+            direction="column"
+        >
             <Header />
-            <Flex h="6vh" />
+            <Flex h={providerUser === 'password' ? '6vh' : '7vh'} />
             <Flex
                 w="100%"
                 h="93%"
@@ -153,6 +161,8 @@ const Profile = () => {
                                 {user.displayName}
                             </Heading>
                             <IconButton
+                            marginLeft="5px"
+                            isRound
                                 variant="ghost"
                                 arial-label="Edit"
                                 colorScheme="whiteAlpha"
@@ -188,52 +198,56 @@ const Profile = () => {
                         borderRadius={10}
                     >
                         <Heading>Danger Zone</Heading>
-                        <FormControl
-                            onKeyDown={(e) => {}}
-                            marginTop="15px"
-                        >
-                            <FormLabel htmlFor="senhaAntiga">
-                                Digite a senha atual
-                            </FormLabel>
-                            <PasswordInput
-                                id="senhaAntiga"
-                                placeholder="Digite sua senha atual"
-                                onChange={(e) => {
-                                    setOldPassword(e.target.value)
-                                }}
-                                value={oldPassword}
-                            />
-                            <FormLabel htmlFor="senha">
-                                Crie uma nova senha:
-                            </FormLabel>
-                            <PasswordInput
-                                id="senha"
-                                placeholder="Digite a nova senha"
-                                onChange={(e) => {
-                                    setPassword(e.target.value)
-                                }}
-                                value={password}
-                            />
-                            <FormLabel htmlFor="confirmaçãoSenha">
-                                Confirme a nova senha:
-                            </FormLabel>
-                            <PasswordInput
-                                id="confirmaçãoSenha"
-                                placeholder="Digite a nova senha novamente"
-                                onChange={(e) => {
-                                    setPasswordConfirm(e.target.value)
-                                }}
-                                value={passwordConfirm}
-                            />
-                            <Button
-                                w="100%"
-                                margin="10px 0px"
-                                colorScheme="yellow"
-                                onClick={() => changePass()}
+                        {providerUser === 'password' ? (
+                            <FormControl
+                                onKeyDown={(e) => {}}
+                                marginTop="15px"
                             >
-                                MUDAR SENHA
-                            </Button>
-                        </FormControl>
+                                <FormLabel htmlFor="senhaAntiga">
+                                    Digite a senha atual
+                                </FormLabel>
+                                <PasswordInput
+                                    id="senhaAntiga"
+                                    placeholder="Digite sua senha atual"
+                                    onChange={(e) => {
+                                        setOldPassword(e.target.value)
+                                    }}
+                                    value={oldPassword}
+                                />
+                                <FormLabel htmlFor="senha">
+                                    Crie uma nova senha:
+                                </FormLabel>
+                                <PasswordInput
+                                    id="senha"
+                                    placeholder="Digite a nova senha"
+                                    onChange={(e) => {
+                                        setPassword(e.target.value)
+                                    }}
+                                    value={password}
+                                />
+                                <FormLabel htmlFor="confirmaçãoSenha">
+                                    Confirme a nova senha:
+                                </FormLabel>
+                                <PasswordInput
+                                    id="confirmaçãoSenha"
+                                    placeholder="Digite a nova senha novamente"
+                                    onChange={(e) => {
+                                        setPasswordConfirm(
+                                            e.target.value
+                                        )
+                                    }}
+                                    value={passwordConfirm}
+                                />
+                                <Button
+                                    w="100%"
+                                    margin="10px 0px"
+                                    colorScheme="yellow"
+                                    onClick={() => changePass()}
+                                >
+                                    MUDAR SENHA
+                                </Button>
+                            </FormControl>
+                        ) : null}
                         <Button
                             w="100%"
                             margin="10px 0px"
