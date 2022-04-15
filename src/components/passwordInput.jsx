f7b960a4312e579d8ca4c5e29bbc86ac31630b78
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-04-15 11:23:34 -0300

    Configura Input para password

--- src/components/passwordInput.jsx
+++ src/components/passwordInput.jsx
@@ -1,9 +1,33 @@
+import { Input, InputGroup, InputRightElement } from '@chakra-ui/react'
 import React from 'react'
+import { RiEyeCloseFill, RiEyeFill } from "react-icons/ri";
 
-const PasswordInput = () => {
-  return (
-    <div>PasswordInput</div>
-  )
+const PasswordInput = (props) => {
+    const [show, setShow] = React.useState(false)
+    const handleClick = () => setShow(!show)
+    return (
+        <InputGroup>
+            <Input
+                type={show ? 'text' : 'password'}
+                id={props.id}
+                placeholder={props.placeholder}
+                margin="0px 0px 20px 0px"
+                color="defaultColor.400"
+                value={props.password}
+                onChange={props.onChange}
+            />
+            <InputRightElement
+                onClick={handleClick}
+                children={
+                    show ? (
+                        <RiEyeFill size={20} color="white" />
+                    ) : (
+                        <RiEyeCloseFill size={20} color="white" />
+                    )
+                }
+            />
+        </InputGroup>
+    )
 }
 
 export default PasswordInput
