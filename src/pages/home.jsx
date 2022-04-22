82151bf1926657d618a59f49a31f58ae23bc1e15
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-04-22 09:41:26 -0300

    Adiciona tela de login enquanto carrega as info

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -20,9 +20,11 @@ import History from 'components/history'
 import SearchPokemons from 'functions/searchPokemons'
 import Header from 'components/header'
 import MyToast from 'components/myToast'
+import LoadingPage from 'components/loadingPage'
 
 export default function Home() {
-    const { setPokemonList, pokemonName, setPokemonName, setPokemonHistory } = useAuth()
+    const { setPokemonList, pokemonName, setPokemonName, setPokemonHistory, loading } =
+        useAuth()
     const isError = pokemonName === ''
     const toast = useToast()
 
@@ -49,83 +51,87 @@ export default function Home() {
         <Flex w="100%" h="100vh" display="flex" direction="column">
             <Header />
             <Flex h="7%" />
-            <Flex w="100%" h="93%" display={{ md: 'flex' }}>
-                <Center
-                    w={{ base: 'full', md: 'mid' }}
-                    h="full"
-                    flexDirection="column"
-                    bg="defaultColor.600"
-                >
-                    <Heading fontFamily="body" color="defaultColor.400">
-                        PokeCoin
-                    </Heading>
-                    <Heading fontFamily="body" color="defaultColor.400">
-                        HUB de Negociação
-                    </Heading>
-                    <FormControl
-                        isRequired
-                        isInvalid={isError}
-                        display="flex"
-                        alignItems="center"
-                        justifyContent="center"
+            {loading ? (
+                <LoadingPage />
+            ) : (
+                <Flex w="100%" h="93%" display={{ md: 'flex' }}>
+                    <Center
+                        w={{ base: 'full', md: 'mid' }}
+                        h="full"
                         flexDirection="column"
-                        margin="40px 0px"
+                        bg="defaultColor.600"
                     >
-                        <FormLabel htmlFor="pokename" color="defaultColor.400">
-                            Comprar pokemon
-                        </FormLabel>
-                        <InputGroup
-                            width="75%"
-                            size="lg"
-                            onKeyDown={(e) => keyboardKey(e)}
+                        <Heading fontFamily="body" color="defaultColor.400">
+                            PokeCoin
+                        </Heading>
+                        <Heading fontFamily="body" color="defaultColor.400">
+                            HUB de Negociação
+                        </Heading>
+                        <FormControl
+                            isRequired
+                            isInvalid={isError}
+                            display="flex"
+                            alignItems="center"
+                            justifyContent="center"
+                            flexDirection="column"
+                            margin="40px 0px"
                         >
-                            <Input
-                                textTransform="lowercase"
-                                onChange={(e) => setPokemonName(e.target.value)}
-                                value={pokemonName}
-                                bg="defaultColor.400"
-                                placeholder="Digite o nome do pokemon que deseja comprar."
-                                id="pokename"
-                                type="text"
-                                margin="0px 0px 15px 0px"
-                            />
-                            <InputRightElement>
-                                <IconButton
-                                    icon={<FaBackspace color="red" size={25} />}
-                                    onClick={() => setPokemonName('')}
+                            <FormLabel htmlFor="pokename" color="defaultColor.400">
+                                Comprar pokemon
+                            </FormLabel>
+                            <InputGroup
+                                width="75%"
+                                size="lg"
+                                onKeyDown={(e) => keyboardKey(e)}
+                            >
+                                <Input
+                                    textTransform="lowercase"
+                                    onChange={(e) => setPokemonName(e.target.value)}
+                                    value={pokemonName}
+                                    bg="defaultColor.400"
+                                    placeholder="Digite o nome do pokemon que deseja comprar."
+                                    id="pokename"
+                                    type="text"
+                                    margin="0px 0px 15px 0px"
                                 />
-                            </InputRightElement>
-                        </InputGroup>
-                        <Button
-                            width="75%"
-                            isDisabled={isError}
-                            leftIcon={<FaBtc />}
-                            colorScheme="green"
-                            id="Compra"
-                            onClick={(event) => buyPokemon(event)}
-                        >
-                            Comprar
-                        </Button>
-                    </FormControl>
-                    <a href="#orders">
-                        <IconButton
-                            colorScheme="red"
-                            icon={<FaArrowCircleDown size={40} color="#f0f0f0" />}
-                            isRound
-                            display={{ md: 'none' }}
-                        />
-                    </a>
-                </Center>
-                <Center
-                    id="orders"
-                    w={{ base: 'full', md: 'mid' }}
-                    h={{ base: '100%', md: '100%' }}
-                    flexDirection="column"
-                >
-                    <Orders />
-                    <History />
-                </Center>
-            </Flex>
+                                <InputRightElement>
+                                    <IconButton
+                                        icon={<FaBackspace color="red" size={25} />}
+                                        onClick={() => setPokemonName('')}
+                                    />
+                                </InputRightElement>
+                            </InputGroup>
+                            <Button
+                                width="75%"
+                                isDisabled={isError}
+                                leftIcon={<FaBtc />}
+                                colorScheme="green"
+                                id="Compra"
+                                onClick={(event) => buyPokemon(event)}
+                            >
+                                Comprar
+                            </Button>
+                        </FormControl>
+                        <a href="#orders">
+                            <IconButton
+                                colorScheme="red"
+                                icon={<FaArrowCircleDown size={40} color="#f0f0f0" />}
+                                isRound
+                                display={{ md: 'none' }}
+                            />
+                        </a>
+                    </Center>
+                    <Center
+                        id="orders"
+                        w={{ base: 'full', md: 'mid' }}
+                        h={{ base: '100%', md: '100%' }}
+                        flexDirection="column"
+                    >
+                        <Orders />
+                        <History />
+                    </Center>
+                </Flex>
+            )}
         </Flex>
     )
 }
