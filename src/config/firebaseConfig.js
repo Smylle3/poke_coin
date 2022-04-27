cecb742a9f862d09b6c4da165c23315a19a23aa5
joao pedro queiroz rodrigues <joao21731@gmail.com>
2022-04-27 14:51:00 -0300

    Faz a chamada para o storage do firevase

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app'
-import { getFirestore } from "firebase/firestore";
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 import {
     getAuth,
     GoogleAuthProvider,
@@ -19,9 +20,10 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
+const storage = getStorage(app)
 const auth = getAuth(app)
 const googleProvider = new GoogleAuthProvider()
 const gitProvider = new GithubAuthProvider()
 const twitterProvider = new TwitterAuthProvider()
 
-export { auth, googleProvider, gitProvider, twitterProvider, app, db }
+export { auth, googleProvider, gitProvider, twitterProvider, app, db, storage }
